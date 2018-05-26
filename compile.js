533e218c6968622154d9a76bab1e7779c7d7cbfe
chrishson <chris.son@mediavalet.com>
2018-05-25 18:36:05 -0700

    added tests for contract

--- compile.js
+++ compile.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync('inboxPath', 'utf8');
+const source = fs.readFileSync(inboxPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
 
